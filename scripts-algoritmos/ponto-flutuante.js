bade85b6fa17cbe07ed9bc23d6c0ed7893081336
Nicholas Sotter <devsotter@gmail.com>
2023-06-24 07:27:08 -0300

    first commit

---
+++ scripts-algoritmos/ponto-flutuante.js
@@ -0,0 +1,17 @@
+// Passo 1: Ler o primeiro número
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+
+// Passo 2: Ler o segundo número
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+// Passo 3: Ler o terceiro número
+let numero3 = parseFloat(prompt("Digite o terceiro número:"));
+
+// Passo 4: Calcular a soma dos três números
+let soma = numero1 + numero2 + numero3;
+
+// Passo 5: Calcular a média
+let media = soma / 3;
+
+// Passo 6: Exibir a média
+console.log("A média dos números é: " + media);
---
+++ scripts-algoritmos/verificar-par.js
@@ -0,0 +1,9 @@
+// Passo 1: Ler o número
+let numero = parseInt(prompt("Digite um número:"));
+
+// Passo 2: Verificar se o número é par ou ímpar
+if (numero % 2 === 0) {
+  console.log(numero + " é um número par.");
+} else {
+  console.log(numero + " é um número ímpar.");
+}
