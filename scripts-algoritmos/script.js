af9fcb3a0679a99463ccfc4c9cd2462afa5e49a2
Nicholas Sotter <devsotter@gmail.com>
2023-06-30 09:55:43 -0300

    novos algoritmos

---
+++ scripts-algoritmos/script.js
@@ -0,0 +1,10 @@
+function calcularFatorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * calcularFatorial(n - 1);
+    }
+  }
+  
+  console.log(calcularFatorial(5)); 
+  
